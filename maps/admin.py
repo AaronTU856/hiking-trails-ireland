dee04c47c267a23200f57439395910cc25dabf3f
AaronTU856 <124066652+AaronTU856@users.noreply.github.com>
2025-10-05 19:53:51 +0100

    Update lab 3: fixes & docs

--- maps/admin.py
+++ maps/admin.py
@@ -1,28 +1,35 @@
-from django.contrib.gis import admin
+from django.contrib import admin as dj_admin
+from django.contrib.gis.admin import GISModelAdmin
+from django.contrib.gis.db.models import PointField, PolygonField
+from django.contrib.gis.forms import OSMWidget
 from .models import Location, TestArea
 
-@admin.register(Location)
-class LocationAdmin(admin.OSMGeoAdmin):
+@dj_admin.register(Location)
+class LocationAdmin(GISModelAdmin):
     list_display = ['name', 'description', 'created_at']
     list_filter = ['created_at']
     search_fields = ['name', 'description']
     readonly_fields = ['created_at']
-    
-    # Map configuration
-    default_zoom = 12
-    display_wkt = True
-    display_srid = True
-    map_width = 600
-    map_height = 400
 
-@admin.register(TestArea)
-class TestAreaAdmin(admin.OSMGeoAdmin):
+    formfield_overrides = {
+        PointField: {"widget": OSMWidget(attrs={
+            "default_zoom": 12,
+            "map_width": 600,
+            "map_height": 400,
+            "display_wkt": True,
+            "display_srid": True,
+        })}
+    }
+
+@dj_admin.register(TestArea)
+class TestAreaAdmin(GISModelAdmin):
     list_display = ['name', 'area_km2']
     readonly_fields = ['area_km2']
-    
-    # Map configuration for polygons
-    default_zoom = 10
-    map_width = 700
-    map_height = 500
 
-
+    formfield_overrides = {
+        PolygonField: {"widget": OSMWidget(attrs={
+            "default_zoom": 10,
+            "map_width": 700,
+            "map_height": 500,
+        })}
+    }
