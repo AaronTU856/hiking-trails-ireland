a5df1ebf1389b8bf331590801d48471d012bfc63
AaronTU856 <124066652+AaronTU856@users.noreply.github.com>
2025-10-31 12:09:12 +0000

    Clean code

--- trails_api/filters.py
+++ trails_api/filters.py
@@ -1,13 +1,15 @@
 import django_filters
 from .models import Trail
 
+# Defines filtering options for the Trail model 
 class TrailFilter(django_filters.FilterSet):
     """Filter set for Trail model"""
-   
+   # Allow case-insensitive search by name, country, or region
     name = django_filters.CharFilter(lookup_expr='icontains')
     country = django_filters.CharFilter(lookup_expr='icontains')
     region = django_filters.CharFilter(lookup_expr='icontains')
    
+   # Filters based on elevation range 
     smallest_mountain = django_filters.NumberFilter(
         field_name='smallest',
         lookup_expr='gte'
@@ -16,7 +18,8 @@ class TrailFilter(django_filters.FilterSet):
         field_name='highest',
         lookup_expr='lte'
     )
-   
+    
+   # Filters for trail length ranges
     longest_trail = django_filters.NumberFilter(
         field_name='longest_trail',
         lookup_expr='gte'
